d5b23e0b585006b0c9e0f811c74ad785bc329564
Chepyshko <Chepyshko@rivne.itstep.org>
2019-12-26 19:43:52 +0200

    smth

---
+++ resources/js/src/App.js
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Header from './components/Header';
+
+import HomePage from './components/Home';
+import RegisterPage from './components/auth/Register';
+
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header />
+                </div>
+                <div className="container">
+                    <Route exact path = "/">
+                    <HomePage />
+                    </Route>
+               
+               
+                    <Route exact path = "/register">
+                    <RegisterPage />
+                    </Route>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ resources/js/src/components/Home/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class HomePage extends Component {
+    render() {
+        return (
+            <>
+
+                <h1>Home Page </h1>
+            </>
+        )
+    }
+}
+
+export default HomePage
